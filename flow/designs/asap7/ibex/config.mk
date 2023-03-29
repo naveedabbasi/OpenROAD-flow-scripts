54f47aa56245b1f8ca81a12910739df590c97889
naveedabbasi <innoxentamo@gmail.com>
2023-03-29 07:57:19 +0500

    improvement Results
    Signed-off-by: naveedabbasi <innoxentamo@gmail.com>

--- flow/designs/asap7/ibex/config.mk
+++ flow/designs/asap7/ibex/config.mk
@@ -6,11 +6,19 @@ export DESIGN_NAME            = ibex_core
 export VERILOG_FILES         = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/*.v))
 export SDC_FILE              = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
-export CORE_UTILIZATION       =  40
-export CORE_ASPECT_RATIO      = 1
-export CORE_MARGIN            = 2
-export PLACE_DENSITY_LB_ADDON  = 0.20
+#export CORE_UTILIZATION       = 55
+#export CORE_ASPECT_RATIO      = 1
+#export CORE_MARGIN            = 2
+#export PLACE_DENSITY_LB_ADDON  = 0.20
 
-export ENABLE_DPO = 0
+#export ENABLE_DPO = 0
+
+
+export DIE_AREA               = 0 0 100 100
+export CORE_AREA              = 2 2 98 98
+
+#export CORE_UTILIZATION       = 30
+export PLACE_DENSITY          = 0.35
 
 export DFF_LIB_FILE           = $($(CORNER)_DFF_LIB_FILE)
+
